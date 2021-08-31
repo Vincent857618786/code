e92323e1a0d3563b96e56d07caff7b1154b43cfb
Vincent <857618786vin@gmail.com>
2021-08-31 09:06:49 +0800

    init

---
+++ 案例/src/cn/edu/zucc/personplan/PersonPlanStarter.java
@@ -0,0 +1,14 @@
+package cn.edu.zucc.personplan;
+
+import cn.edu.zucc.personplan.ui.FrmMain;
+
+public class PersonPlanStarter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new FrmMain();
+	}
+
+}
